701b914557bef9175f8d5f4086fc8c004880d9a2
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 16:46:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,6 @@
 class School
   def school_name=(school_name)
-      attr_accessor :name, :grade
- end def School
-   school = School.new("Bayside High School")
+      school = School.new("Bayside High School")
 
 end
 end
