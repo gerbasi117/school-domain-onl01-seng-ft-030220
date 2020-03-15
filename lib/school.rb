01c1844803d72adf25a4f67c94e756fe2757977a
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 16:54:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,4 +4,9 @@ def initialize(name)
   @name = name
   @roster = {}
    end 
-   
+   def add_student(student, level)
+     # this sort of means #=> a || a = b ... or #=> x || x = y
+     # it's freak'n weird
+    roster[level] ||= []
+     roster[level] << student
+   end 
