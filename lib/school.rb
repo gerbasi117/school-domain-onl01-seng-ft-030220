c4a73aba63e91d625f0171add1be49fc2bb3209d
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 17:00:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,12 @@ def initialize(name)
     roster[level] ||= []
      roster[level] << student
    end 
-  def grade(level)
-     roster.detect do |x, y| 
-       if x == level
-         return y 
-       end
+def sort 
+   nu_hash = {}
+   roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+    end 
+   nu_hash
+ end
    end
  
